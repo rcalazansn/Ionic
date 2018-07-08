e084dc4776dd8944cee4d9c5043202a8c0f00857
Rodrigo Calazans Negrao <calazans@MacBook-Air-de-Rodrigo.local>
2018-07-08 18:01:12 -0300

    projeto listagem de filmes

---
+++ src/pages/feed/feed.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-feed',
+  templateUrl: 'feed.html',
+})
+export class FeedPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad FeedPage');
+  }
+
+}
